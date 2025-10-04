83d8260ed6bf1a888825027b179b2950bcaefee7
seoyunje <origin6165@naver.com>
2025-10-04 10:28:08 +0900

    Add SAM Module in attention.py

--- attention.py
+++ attention.py
@@ -24,6 +24,26 @@ class SEBlock(nn.Module):
         out = self.fc(out)
 
         return x * self.sigmoid(out)
+    
+## Spatial Attention Module
+class SAM(nn.Module):
+    def __init__(self, kernel_size=3):
+        super(SAM, self).__init__()
+
+        self.conv = nn.Conv2d(2, 1, kernel_size=3, padding='same', bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    
+    def forward(self, x):
+
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+
+        cat = torch.cat([max_out, avg_out], dim=1)
+
+        out = self.conv(cat)
+        
+        return x * self.sigmoid(out)
 
 ## Channel Attention Module
 class CAM(nn.Module):
