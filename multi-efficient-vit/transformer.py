03377816ce3fe333dee3950a379182bd56808e7e
seoyunje <origin6165@naver.com>
2025-11-22 18:46:28 +0900

    ✨ Create transformer.py

---
+++ multi-efficient-vit/transformer.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+from .attention import MultiHeadAttention
+
+class TransformerBlock(nn.Module):
+    def __init__(self,
+                 dim,
+                 num_heads,
+                 mlp_ratio=4.,
+                 qkv_bias=False,
+                 attn_drop=0.,
+                 drop=0.,
+                ):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(dim)
+        self.attn = MultiHeadAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
+        self.norm2 = nn.LayerNorm(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = nn.Sequential(
+            nn.Linear(dim, mlp_hidden_dim),
+            nn.GELU(),
+            nn.Dropout(drop),
+            nn.Linear(mlp_hidden_dim, dim),
+            nn.Dropout(drop)
+        )
+
+    def forward(self, x):
+        x = x + self.attn(self.norm1(x))
+        x = x + self.mlp(self.norm2(x))
+        return x
