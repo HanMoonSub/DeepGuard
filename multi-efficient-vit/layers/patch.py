ef076be9994446062a707a59c44f274fff53ba8d
seoyunje <origin6165@naver.com>
2025-12-31 12:09:53 +0900

    ✨ Add Cls Token & Pos Embed

--- multi-efficient-vit/layers/patch.py
+++ multi-efficient-vit/layers/patch.py
@@ -1,5 +1,6 @@
+import torch
 import torch.nn as nn
-from typing import Type, Optional
+from typing import Type, Optional, List
 from .reducesize import ReduceSize
 from .se import SE
 
@@ -41,16 +42,32 @@ class MidLevelPatchEmbed(nn.Module):
                 in_chs: int,
                 dim: int,
                 block_idx: int,
+                input_resolution: List[int]
                 ):
         super().__init__()
         
+        """
+        Args:
+            in_chs: Feature Map Channels
+            dim: Embedding Dimension
+            block_idx: Backbone Block index
+            input_resolution: Feature map Height, Width
+        """
+        
+        H, W = input_resolution
+        self.cls_token = nn.Parameter(torch.zeros(1,1,dim))
+        
         if block_idx == 0:
             self.proj = nn.Conv2d(in_chs, dim, kernel_size=7, stride=4, padding=3)
+            n_h, n_w = H // 8, W // 8
         elif block_idx == 1:
             self.proj = nn.Conv2d(in_chs, dim, kernel_size=3, stride=2, padding=1)
+            n_h, n_w = H // 4, W // 4
         elif block_idx == 2:
             self.proj = nn.Conv2d(in_chs, dim, kernel_size=1, stride=1, padding=0)
+            n_h, n_w = H // 2, W // 2
         
+        self.pos_embed = nn.Parameter(torch.zeros(1, n_h * n_w + 1, dim))    
         self.conv_down = ReduceSize(dim=dim, keep_dim=True)
         
     def forward(self, x):
@@ -58,14 +75,22 @@ class MidLevelPatchEmbed(nn.Module):
         x = x.permute(0,2,3,1) # (B,C,H,W) -> (B,H,W,C)
         x = self.conv_down(x)
         
-        return x
+        B,H,W,C = x.shape
+        x = x.reshape(B,-1,C)
+        
+        cls_token = self.cls_token.expand(B,-1,-1)
+        x = torch.cat((cls_token, x), dim=1)
+        
+        x += self.pos_embed
+        
+        return x # (B,N+1,C)
     
     
 class LowLevelPatchEmbed(nn.Module):
     
     """
     Feature map: block.5 output, (B,C,H,W) -> (B,H//2,W//2,D)
-    Feature map: block.6 output, (B,C,H,W) -> (B,H,W,D
+    Feature map: block.6 output, (B,C,H,W) -> (B,H,W,D)
     
     """
     
@@ -75,21 +100,45 @@ class LowLevelPatchEmbed(nn.Module):
                 in_chs: int,
                 dim: int, 
                 block_idx: int, 
+                input_resolution: List[int]
                 ):
         super().__init__()
+        
+        """
+        Args:
+            in_chs: Feature Map Channels
+            dim: Embedding Dimension
+            block_idx: Backbone Block index
+            input_resolution: Feature map Height, Width
+        """
+        
+        H, W = input_resolution
+        self.cls_token = nn.Parameter(torch.zeros(1,1,dim))
         
         if block_idx == 5:
             self.block = nn.Sequential(
                 FeatExtract(in_chs),
                 nn.Conv2d(in_chs, dim, kernel_size=1, stride=1, padding=0)
             )
+            n_h, n_w = H // 2, W // 2
         
         elif block_idx == 6:
             self.block = nn.Conv2d(in_chs, dim, kernel_size=1, stride=1, padding=0)
-        
+            n_h, n_w = H, W
+            
+        self.pos_embed = nn.Parameter(torch.zeros(1, n_h * n_w + 1, dim))    
+            
     def forward(self, x):
         
         x = self.block(x)
         x = x.permute(0,2,3,1).contiguous() # (B,C,H,W) -> (B,H,W,C)
         
-        return x
+        B,H,W,C = x.shape
+        x = x.reshape(B,-1,C) # (B,N,C)
+        
+        cls_token = self.cls_token.expand(B,-1,-1)
+        x = torch.cat((cls_token, x), dim=1)    
+            
+        x += self.pos_embed
+        
+        return x # (B,N+1,C)
