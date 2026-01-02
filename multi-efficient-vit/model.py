7490356298fa18ee658b69a466480e52d21884d6
seoyunje <origin6165@naver.com>
2026-01-02 11:55:52 +0900

    ✨ Refactory model.py

--- multi-efficient-vit/model.py
+++ multi-efficient-vit/model.py
@@ -1,63 +1,76 @@
+import torch
 import torch.nn as nn
-from .branch import EfficientBranch
-from .encoder import MultiScaleEncoder
+from .layers.featextractor import FeatExtractor
+from .layers.transformer import ViTLayer
+from typing import List, Union
 
 class MultiScaleEffViT(nn.Module):
-    def __init__(self, config):
-
+    def __init__(
+            self,
+            model_name: str,
+            img_size: List[int],
+            l_block_idx: int, 
+            h_block_idx: int,
+            l_dim: int = 256,
+            h_dim: int = 512, 
+            l_depth: int = 2,
+            h_depth: int = 4,
+            l_heads: int = 4,
+            h_heads: int = 8, 
+            l_ratio: Union[float, int] = 4.,
+            h_ratio: Union[float, int] = 4.,
+            l_drop: float = 0.,
+            h_drop: float = 0.,
+            l_attn_drop: float = 0.,
+            h_attn_drop: float = 0.,
+            l_drop_path: float = 0.,
+            h_drop_path: float = 0.,
+            **kwargs
+            ):
         super().__init__()
-
-        self.config = config
-
-        self.cnn_branch = EfficientBranch(self.config.cnn_backbone, self.config.img_size, self.config.h_branch, self.config.m_branch)
-
-        self.multi_encoder = MultiScaleEncoder(self.config.h_branch, self.config.m_branch)
-
-        # Head
-        self.h_branch_head = self.create_branch_head(self.config.h_branch.embed_dim)
-        self.m_branch_head = self.create_branch_head(self.config.m_branch.embed_dim)
-
-        self._init_weights()
-
-    def _init_weights(self):
-        modules_to_init = [
-            self.multi_encoder,
-            self.h_branch_head,
-            self.m_branch_head,
-        ]
-
-        for module in modules_to_init:
-            for m in module.modules():
-                if isinstance(m, nn.Linear):
-                    nn.init.trunc_normal_(m.weight, std=0.02)
-                    if m.bias is not None:
-                        nn.init.constant_(m.bias, 0)
-                elif isinstance(m, nn.LayerNorm):
-                    nn.init.constant_(m.bias, 0)
-                    nn.init.constant_(m.weight, 1.0)
-
-    @staticmethod
-    def create_branch_head(embed_dim, dropout=0.5):
-        return nn.Sequential(
-            nn.LayerNorm(embed_dim),
-            nn.Linear(embed_dim, embed_dim // 2),
-            nn.LayerNorm(embed_dim // 2),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(embed_dim // 2, embed_dim // 4),
-            nn.LayerNorm(embed_dim // 4),
-            nn.GELU(),
-            nn.Dropout(dropout/2),
-            nn.Linear(embed_dim // 4, 1)
-            )
-
+        
+        self.feat_extractor = FeatExtractor(
+                            model_name = model_name, 
+                            img_size = img_size, 
+                            l_block_idx = l_block_idx, 
+                            h_block_idx = h_block_idx
+                            )
+        
+        with torch.no_grad():
+            dummy_input = torch.zeros(1,3,*img_size)
+            l_result, h_result = self.feat_extractor(dummy_input)
+        
+        self.l_vit = ViTLayer(
+                            in_chs = l_result['in_chs'], 
+                            block_idx = l_result['block_idx'], 
+                            input_resolution = l_result['input_resolution'], 
+                            dim = l_dim, 
+                            depth = l_depth, 
+                            num_heads = l_heads, 
+                            mlp_ratio = l_ratio,
+                            drop = l_drop,
+                            attn_drop = l_attn_drop,
+                            drop_path = l_drop_path,
+                            patch_embed = 'low')
+        
+        self.h_vit = ViTLayer(
+                            in_chs = h_result['in_chs'], 
+                            block_idx = h_result['block_idx'], 
+                            input_resolution = h_result['input_resolution'], 
+                            dim = h_dim, 
+                            depth = h_depth, 
+                            num_heads = h_heads, 
+                            mlp_ratio = h_ratio,
+                            drop = h_drop,
+                            attn_drop = h_attn_drop,
+                            drop_path = h_drop_path,
+                            patch_embed = 'high')
+        
     def forward(self, x):
-
-        h_branch_token, m_branch_token, = self.cnn_branch(x)
-
-        h_branch_out, m_branch_out = self.multi_encoder(h_branch_token, m_branch_token)
-
-        h_branch_out = self.h_branch_head(h_branch_out[:,0,:]) # cls_token
-        m_branch_out = self.m_branch_head(m_branch_out[:,0,:]) # cls_token
-
-        return h_branch_out + m_branch_out
+        
+        l_result, h_result = self.feat_extractor(x) # (B,C,H,W)
+        
+        l_out = self.l_vit(l_result['feature_map']) # (B,num_classes)
+        h_out = self.h_vit(h_result['feature_map']) # (B,num_classes)
+    
+        return l_out + h_out
