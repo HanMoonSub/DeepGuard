33aff2bc4a543d6f20700b80f706b454b6a3ad36
seoyunje <origin6165@naver.com>
2026-01-03 11:35:58 +0900

    ♻️ Add timm registry support and update model config

--- multi_efficient_vit/model.py
+++ multi_efficient_vit/model.py
@@ -3,14 +3,44 @@ import torch.nn as nn
 from .layers.featextractor import FeatExtractor
 from .layers.transformer import ViTLayer
 from typing import List, Union
+from timm.models._builder import build_model_with_cfg
+from timm.models.registry import register_model
 
+def _cfg(url='', **kwargs):
+    return {
+        'url': url,
+        'num_classes': 1, # Real or Fake 
+        'input_size': (3, 224, 224), 
+        'pool_size': None,
+        'crop_pct': 0.875, 
+        'interpolation': 'bicubic', 
+        'fixed_input_size': True,
+        'mean': (0.485, 0.456, 0.406), 
+        'std': (0.229, 0.224, 0.225),
+        **kwargs
+    }
+    
+default_cfgs = {
+    'ms_eff_vit_b0': _cfg(
+            url = '', # https://github.com/HanMoonSub/DeepGuard/releases/
+            input_size = (3,224,224)
+        ),
+    'ms_eff_vit_b5': _cfg(
+            url = '', # https://github.com/HanMoonSub/DeepGuard/releases/
+            input_size = (3,384,384)
+        ),
+    'ms_eff_vit_b7': _cfg(
+            url = '', # https://github.com/HanMoonSub/DeepGuard/releases/
+            input_size = (3,512,512)
+        ),
+}
 class MultiScaleEffViT(nn.Module):
     def __init__(
             self,
             model_name: str,
             img_size: List[int],
-            l_block_idx: int, 
-            h_block_idx: int,
+            l_block_idx: int = 1, 
+            h_block_idx: int = 6,
             l_dim: int = 256,
             h_dim: int = 512, 
             l_depth: int = 2,
@@ -100,3 +130,76 @@ class MultiScaleEffViT(nn.Module):
         h_out = self.h_vit(h_feat) # (B,num_classes)
     
         return l_out + h_out
+    
+    
+def _create_ms_eff_vit(variant, pretrained=False, **kwargs):
+    
+    return build_model_with_cfg(
+        MultiScaleEffViT,
+        variant,
+        pretrained,
+        **kwargs
+    )
+    
+
+@register_model
+def ms_eff_vit_b0(pretrained=False, **kwargs) -> MultiScaleEffViT:
+    model_kwargs = dict(
+        model_name = "tf_efficientnet_b0.ns_jft_in1k",
+        img_size = [224,224],
+        l_dim = 192,
+        h_dim = 384, 
+        l_depth = 2,
+        h_depth = 3,
+        l_heads = 3,
+        h_heads = 6, 
+        **kwargs
+    )
+    return _create_ms_eff_vit('ms_eff_vit_b0', pretrained=pretrained, **model_kwargs)
+
+@register_model
+def ms_eff_vit_b5(pretrained=False, **kwargs) -> MultiScaleEffViT:
+    model_kwargs = dict(
+        model_name = "tf_efficientnet_b5.ns_jft_in1k",
+        img_size = [384,384],
+        l_dim = 256,
+        h_dim = 768, 
+        l_depth = 3,
+        h_depth = 4,
+        l_heads = 4,
+        h_heads = 12, 
+        l_ratio = 4.,
+        h_ratio = 3.,
+        l_drop = 0.05,
+        h_drop = 0.1,
+        l_attn_drop = 0.05,
+        h_attn_drop = 0.05,
+        l_drop_path = 0.05,
+        h_drop_path = 0.1,
+        **kwargs
+    )
+    return _create_ms_eff_vit('ms_eff_vit_b5', pretrained=pretrained, **model_kwargs)
+
+@register_model
+def ms_eff_vit_b7(pretrained=False, **kwargs) -> MultiScaleEffViT:
+    model_kwargs = dict(
+        model_name = "tf_efficientnet_b7.ns_jft_in1k",
+        img_size = [384,384],
+        l_dim = 384,
+        h_dim = 768, 
+        l_depth = 3,
+        h_depth = 5,
+        l_heads = 6,
+        h_heads = 12, 
+        l_ratio = 4.,
+        h_ratio = 3.,
+        l_drop = 0.1,
+        h_drop = 0.2,
+        l_attn_drop = 0.1,
+        h_attn_drop = 0.1,
+        l_drop_path = 0.1,
+        h_drop_path = 0.2,
+        **kwargs
+    )
+    return _create_ms_eff_vit('ms_eff_vit_b7', pretrained=pretrained, **model_kwargs)
+    
