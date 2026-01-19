92f20e305262cd2f0dfe6d927de48c1b44bd8fc2
seoyunje <origin6165@naver.com>
2026-01-19 10:30:41 +0900

    ♻️ Refactory transforms.py

--- deepguard/data/__init__.py
+++ deepguard/data/__init__.py
@@ -1,4 +1,8 @@
 from .deepfake_dataset import DeepFakeDataset
-from .transforms import get_transform
+from .transforms import (
+    get_train_transforms,
+    get_valid_transforms,
+    get_test_transforms,
+)
 from .handle_imbalance import class_imbalance_handle
 from .split_data import split_data
--- deepguard/data/transforms.py
+++ deepguard/data/transforms.py
@@ -1,54 +1,76 @@
+from typing import List
 import cv2
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
-def get_transform(cfg, 
-                  mode: str = "train", 
-                  tta_hflip: bool = False,
-                  tta_scale: float = 1.0):
+def _get_isotropical_resize(img_size: List[int]):
+    h, w = img_size
+    return [
+        A.LongestMaxSize(max_size=max(h, w)),
+        A.PadIfNeeded(min_height=h, min_width=w,
+                      border_mode=cv2.BORDER_CONSTANT, fill_value=0)
+    ]
+
+def _get_normalization():
+    return [
+        A.Normalize(mean=(0.485, 0.456, 0.406),
+                    std=(0.229, 0.224, 0.225)),
+        ToTensorV2(),
+    ]
+
+def get_train_transforms(img_size: List[int]):
     """
-    Returns an Albumentations Compose transform.
-
-    Args:
-        cfg: config object with img_size, mean, std
-        mode (str): 'train', 'valid', 'test'
-        tta_hflip (bool): apply horizontal flip (TTA option)
-        tta_scale (float): scale factor for resizing (TTA option)
+        Create a data augmentation pipeline for model training
     """
-    mode = str(mode).lower()
-    if mode not in ["train", "valid", "test"]:
-        raise ValueError("mode must be one of ['train', 'valid', 'test']")
+    transforms = [
+        *_get_isotropical_resize(img_size),
+        A.HorizontalFlip(p=0.5),
+        A.Affine(translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)}, scale=(0.8, 1.2), rotate=(-10, 10), mode=cv2.BORDER_CONSTANT, cval=0, p=0.5),
+        A.RandomBrightnessContrast(brightness_limit=0.15, contrast_limit=0.15, p=0.25),
+    ]
     
-    
-    transforms_list = []
-    
-    if not hasattr(cfg, "img_size"):
-        raise ValueError("You need to Img Size for Image Augmentation")
-    
-    if not hasattr(cfg, "mean") or not hasattr(cfg, "std"):
-        raise ValueError("You need to mean, std for Image Preprocessing")
-    
-
-    # Resize
-    if tta_scale != 1.0:
-        h, w = cfg.img_size
-        new_h, new_w = int(h * tta_scale), int(w * tta_scale)
-        transforms_list.append(A.Resize(height=new_h, width=new_w))
-
-    # Horizontal flip
-    if mode == "train":
-        transforms_list.append(A.HorizontalFlip(p=0.5))
-        transforms_list.append(A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2, rotate_limit=10, border_mode=cv2.BORDER_CONSTANT, p=0.5))
-        transforms_list.append(A.RandomBrightnessContrast(brightness_limit=0.15, contrast_limit=0.15, p=0.3))
-    elif mode == "valid":
-        pass
-    elif mode == "test" and tta_hflip:
-        transforms_list.append(A.HorizontalFlip(p=0.5))
-
-    # Normalize + ToTensor
-    transforms_list.extend([
-        A.Normalize(mean=cfg.mean, std=cfg.std),
-        ToTensorV2()
+    return A.Compose([
+        *transforms,
+        *_get_normalization(),
     ])
 
-    return A.Compose(transforms_list)
+def get_valid_transforms(img_size: List[int]):
+    """
+        Create a minimal preprocessing pipeline for validation
+    """
+    
+    return A.Compose([
+        *_get_isotropical_resize(img_size),
+        *_get_normalization()
+    ])
+
+def get_test_transforms(
+                        img_size: List[int],
+                        tta_hflip: float = 0.0,
+                        tta_scale: float = 1.0,
+                    ):  
+    """ Creates a pipeline for inference and Test Time Augmentation (TTA).
+
+    Args:
+        img_size (List[int]): Base image size [height, width].
+        tta_hflip (float): Probability of applying a horizontal flip. Defaults to 0.0.
+        tta_scale (float): Scaling factor for the image. Defaults to 1.0.
+
+    Returns:
+        A.Compose: Pipeline with TTA options and normalization.
+    """
+
+    
+    transforms = _get_isotropical_resize(img_size)
+        
+    if tta_scale != 1.0:
+        h, w = img_size        
+        transforms.append(A.Resize(height=int(h * tta_scale), width=int(w * tta_scale)))
+    
+    if tta_hflip != 0:
+        transforms.append(A.HorizontalFlip(p=tta_hflip))
+        
+    return A.Compose([
+        *transforms,
+        *_get_normalization(),
+        ])
