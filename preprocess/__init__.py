5ced70af5459548fb7943af37c402ccfbe62cec7
seoyunje <origin6165@naver.com>
2026-02-11 11:28:23 +0900

    ✨ Create FaceDetector_KODF

--- preprocess/__init__.py
+++ preprocess/__init__.py
@@ -1,3 +1,3 @@
-from .face_detector import FaceDetector
+from .face_detector import FaceDetector, FaceDetector_KODF
 from .frame_extractor import FrameExtractor, FrameExtractor_KODF
 from .landmark_detector import LandmarkDetector
