c5f0ebe828cd11819dd43409838eed567da3844a
seoyunje <origin6165@naver.com>
2026-03-13 17:30:48 +0900

    ✨ Create common.py

--- App/main.py
+++ App/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from routes import auth
+from utils.common import lifespan
 
 # 가상 인스턴스 생성
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth.router)
