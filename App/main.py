a3fa1197605480b3b7fba5a4f6d380849bdf54f6
seoyunje <origin6165@naver.com>
2026-03-13 17:41:04 +0900

    ✨ Add CORSMiddleware

--- App/main.py
+++ App/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from routes import auth
 from utils.common import lifespan
 from utils import exc_handler
 
 # 가상 인스턴스 생성
 app = FastAPI(lifespan=lifespan)
+
+# CORSMiddleware
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   max_age = -1
+                   )
 
 app.include_router(auth.router)
 
