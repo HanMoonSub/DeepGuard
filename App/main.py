39a07db587ab8241972447f468979cc5402c0daa
seoyunje <origin6165@naver.com>
2026-03-13 17:40:04 +0900

    ✨ Create exc_handler.py

--- App/main.py
+++ App/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from routes import auth
 from utils.common import lifespan
+from utils import exc_handler
 
 # 가상 인스턴스 생성
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth.router)
+
+# Custom HTTPException Handler
+app.add_exception_handler(StarletteHTTPException, exc_handler.custom_http_exception_handler)
+# Custom RequestValidationError Handler
+app.add_exception_handler(RequestValidationError, exc_handler.validation_exception_handler)
---
+++ App/utils/exc_handler.py
@@ -0,0 +1,26 @@
+from fastapi import Request, status
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+
+# HTTPException 오류 발생 시 커스텀 exception handler 로직
+async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
+    return JSONResponse(
+        content = {
+            "status_code": exc.status_code,
+            "title_message": "불편을 드려 죄송합니다",
+            "detail": exc.detail},
+        status_code = exc.status_code
+    )
+    
+# RequestValidationError 오류 발생 시 커스텀 exception handler 로직
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        content = {
+            "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "title_message": "잘못된 값을 입력하였습니다",
+            "detail": exc.errors()
+            },
+        status_code = status.HTTP_422_UNPROCESSABLE_CONTENT
+    )
