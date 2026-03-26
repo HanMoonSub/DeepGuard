5a9d9f1729bbfe1fbcde630441644b9121bb8c82
HanMoonSub <hanmoon3054@gmail.com>
2026-03-26 09:48:23 +0900

    Add error_type in exc_handler.py

--- App/utils/exc_handler.py
+++ App/utils/exc_handler.py
@@ -8,6 +8,7 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
     return JSONResponse(
         content = {
+            "error_type" : "http",
             "status_code": exc.status_code,
             "title_message": "불편을 드려 죄송합니다",
             "detail": exc.detail},
@@ -18,6 +19,7 @@ async def custom_http_exception_handler(request: Request, exc: StarletteHTTPExce
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     return JSONResponse(
         content = {
+            "error_type" : "vaild",
             "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT,
             "title_message": "잘못된 값을 입력하였습니다",
             "detail": exc.errors()
