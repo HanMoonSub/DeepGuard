1ccba748936efa0d10dcaa305a57031518a5ea63
seoyunje <origin6165@naver.com>
2026-04-03 18:12:39 +0900

    🔇 Suppress unnecessary library logs and add HF_TOKEN

--- deepguard/__init__.py
+++ deepguard/__init__.py
@@ -1,3 +1,11 @@
+import warnings
+import logging
+
+# ------- 로그 및 경고 제어 ---------
+warnings.filterwarnings("ignore") # 일반 경고 숨기기
+logging.getLogger("timm").setLevel(logging.ERROR) # timm 내부 로그 숨기기
+logging.getLogger("torch").setLevel(logging.ERROR) # torch 관련 로그 숨기기
+
 from .models import ms_eff_vit, ms_eff_gcvit
 from .models.ms_eff_vit import (
     MultiScaleEffViT,
@@ -9,3 +17,12 @@ from .models.ms_eff_gcvit import (
     ms_eff_gcvit_b0,
     ms_eff_gcvit_b5,
 )
+
+__all__ = [
+    "MultiScaleEffViT",
+    "ms_eff_vit_b0",
+    "ms_eff_vit_b5",
+    "MultiScaleEffGCViT",
+    "ms_eff_gcvit_b0",
+    "ms_eff_gcvit_b5",
+]
