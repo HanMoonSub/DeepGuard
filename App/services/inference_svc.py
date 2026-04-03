859d3787e7e48e0f52a39c20de9676db80812205
seoyunje <origin6165@naver.com>
2026-04-03 18:06:24 +0900

    ✨ Add cache_key

--- App/services/inference_svc.py
+++ App/services/inference_svc.py
@@ -1,10 +1,10 @@
 import timm
 from inference.image_predictor import ImagePredictor
 
+model_cache = {}
+
 # ----- 사용자 업로드 Image 딥페이크 여부 분석 ------
 async def predict_image(image_loc: str, model_type: str, domain_type: str):
-    
-    print(f"model_type: {model_type}, domain_type: {domain_type}")
     
     model_name = None
     dataset = None
@@ -21,17 +21,18 @@ async def predict_image(image_loc: str, model_type: str, domain_type: str):
             model_name = "ms_eff_gcvit_b5"; dataset = "ff++"
         else:
             model_name = "ms_eff_gcvit_b5"; dataset = "kodf"
-            
-    print(f"model_name: {model_name}, dataset: {dataset}")
-            
-    # Image Predictor 초기화 진행
-    predictor = ImagePredictor(
-            margin_ratio = 0.2, #  Margin ratio around the detected face crop
-            conf_thres = 0.5, # Confidence threshold for face detection
-            min_face_ratio = 0.01, # Minimum face-toframe size ratio to process 
+    
+    cache_key = (model_name, dataset)
+    
+    if cache_key not in model_cache:
+        model_cache[cache_key] = ImagePredictor(
+            margin_ratio = 0.2,
+            conf_thres = 0.5,
+            min_face_ratio = 0.01,
             model_name = model_name,   
-            dataset = dataset 
-            )
+            dataset = dataset )
+        
+    predictor = model_cache[cache_key]
     
     result = predictor.predict_img(
         img_path = f"./{image_loc}",
