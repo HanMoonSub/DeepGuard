bbffba43334081e6c20973f838f234e466006134
seoyunje <origin6165@naver.com>
2026-04-03 16:59:34 +0900

    🎨 Improve init.py in deepguard

--- deepguard/__init__.py
+++ deepguard/__init__.py
@@ -1,2 +1,11 @@
-from .layers import *
-from .models import *
+from .models import ms_eff_vit, ms_eff_gcvit
+from .models.ms_eff_vit import (
+    MultiScaleEffViT,
+    ms_eff_vit_b0,
+    ms_eff_vit_b5,
+)
+from .models.ms_eff_gcvit import (
+    MultiScaleEffGCViT,
+    ms_eff_gcvit_b0,
+    ms_eff_gcvit_b5,
+)
