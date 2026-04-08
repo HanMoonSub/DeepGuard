6ac5b8c8a3cc5f8aebbf48945ce541a5a649b1bb
seoyunje <origin6165@naver.com>
2026-04-09 08:52:03 +0900

    ⚡️ Add face info & result_msg in image_result.db

--- App/sql/initial_image_result.sql
+++ App/sql/initial_image_result.sql
@@ -6,9 +6,13 @@ create table deepfake_db.image_result (
     image_loc varchar(300) not null unique,
     label varchar(10) not null,
     score float not null,
+    face_conf float not null,
+    face_ratio float not null,
+    face_brightness float not null,
     version_type varchar(10) not null,
     model_type varchar(10) not null,
     domain_type varchar(20) not null,
+    result_msg varchar(200) not null,
     created_at timestamp default current_timestamp,
     
     index user_id_idx(user_id)
