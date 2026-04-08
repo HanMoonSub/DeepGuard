18863c552fd9e1ce223ecd2c4f55c442dd6c4bdf
seoyunje <origin6165@naver.com>
2026-04-09 08:53:44 +0900

    🎨 Update new parameter & Error Handling

--- App/services/inference_svc.py
+++ App/services/inference_svc.py
@@ -43,7 +43,6 @@ async def predict_image(image_loc: str, version_type: str, model_type: str, doma
         model_cache[cache_key] = ImagePredictor(
             margin_ratio=0.2,
             conf_thres=0.5,
-            min_face_ratio=0.01,
             model_name=model_name,
             dataset=dataset
         )
@@ -53,25 +52,25 @@ async def predict_image(image_loc: str, version_type: str, model_type: str, doma
     # 비동기 이미지 추론 실행
     loop = asyncio.get_running_loop()
     try:
-        prob = await loop.run_in_executor(
+        analysis = await loop.run_in_executor(
             None, 
             predictor.predict_img, 
             f"./{image_loc}", 
             0.0
         )
         
-        print("딥페이크 확률 값: ", prob)
+        print(f"딥페이크 확률 값: {analysis["prob"]}, 얼굴 신뢰도: {analysis["face_conf"]}, 얼굴 비율: {analysis["face_ratio"]}, 얼굴 밝기: {analysis["face_brightness"]}")
     
         # 분석 성공 시
         return {
-            "prob": prob,
+            "analysis": analysis,
             "message": "이미지 분석에 성공하였습니다",
             "status": "success"
         }
     except PredictorError as e:
         print(e.message)
         return {
-            "prob": -1,
+            "analysis": {"prob": -1, "face_conf": -1, "face_ratio": -1, "face_brightness": -1},
             "message": e.message,
             "status": "warning",
         }
@@ -85,8 +84,8 @@ async def predict_image(image_loc: str, version_type: str, model_type: str, doma
         )
 
 # 이미지 메타데이터 + 추론 결과값 DB에 저장
-async def register_image_result(conn: Connection, user_id: int, image_loc: str, score: float,
-                                version_type: str, model_type: str, domain_type: str):
+async def register_image_result(conn: Connection, user_id: int, image_loc: str, score: float, face_conf: float, face_ratio: float,
+                                face_brightness: float, version_type: str, model_type: str, domain_type: str, result_msg: str):
     if score > 0.5: # 0.5 ~ 1.0 구간 
         label = "FAKE"
     elif score >= 0: # 0.0 ~ 0.5 구간
@@ -96,13 +95,13 @@ async def register_image_result(conn: Connection, user_id: int, image_loc: str, 
         
     try:
         query = """
-        INSERT INTO image_result(user_id, image_loc, label, score, version_type, model_type, domain_type)
-        values (:user_id, :image_loc, :label, :score, :version_type, :model_type, :domain_type)
+        INSERT INTO image_result(user_id, image_loc, label, score, face_conf, face_ratio, face_brightness, version_type, model_type, domain_type, result_msg)
+        values (:user_id, :image_loc, :label, :score, :face_conf, :face_ratio, :face_brightness, :version_type, :model_type, :domain_type, :result_msg)
         """
         
         stmt = text(query)
-        bind_stmt = stmt.bindparams(user_id=user_id, image_loc=image_loc, label=label, score=score,
-                                    version_type=version_type, model_type=model_type, domain_type=domain_type)
+        bind_stmt = stmt.bindparams(user_id=user_id, image_loc=image_loc, label=label, score=score, face_conf=face_conf, face_ratio=face_ratio,
+                                    face_brightness=face_brightness, version_type=version_type, model_type=model_type, domain_type=domain_type, result_msg=result_msg)
         await conn.execute(bind_stmt)
         await conn.commit()
         
