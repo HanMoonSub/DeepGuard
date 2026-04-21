9cc6e502d50199a32edd38223a857d9cfa98f8f3
seoyunje <origin6165@naver.com>
2026-04-22 08:44:59 +0900

    ♻️ Refactor Auth Route & Service

--- App/routes/auth.py
+++ App/routes/auth.py
@@ -10,11 +10,11 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 # --- 회원가입 API ---
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register_user(request: Request,
-                        req: RegisterRequest,
+                        user_info: RegisterRequest,
                         conn: Connection = Depends(context_get_conn)):
     
     # 1. 이메일 중복 확인
-    user = await auth_svc.get_user_by_email(conn=conn, email=req.email)
+    user = await auth_svc.get_user_by_email(conn=conn, email=user_info.email)
     if user is not None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -22,27 +22,26 @@ async def register_user(request: Request,
         )
     
     # 2. 비밀번호 암호화 (해싱)
-    hashed_password = auth_svc.get_hashed_password(req.password)
+    hashed_password = auth_svc.get_hashed_password(user_info.password)
     
     # 3. 신규 유저 DB 저장
     await auth_svc.register_user(
         conn=conn, 
-        name=req.name, 
-        email=req.email,
+        name=user_info.name, 
+        email=user_info.email,
         hashed_password=hashed_password
     )
     
     return {"message": "회원가입이 성공적으로 완료되었습니다.", "status": "success"}
 
-
 # --- 로그인 API ---
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def login_user(request: Request,
-                     req: LoginRequest,
+                     login_info: LoginRequest,
                      conn: Connection = Depends(context_get_conn)):
     
     # 1. 유저 인증 (DB 조회 및 비밀번호 대조)
-    user = await auth_svc.authenticate_user(conn=conn, email=req.email)
+    user = await auth_svc.authenticate_user(conn=conn, email=login_info.email)
     # 2. 인증 실패 처리
     if not user:
         raise HTTPException(
@@ -50,7 +49,7 @@ async def login_user(request: Request,
             detail="해당 이메일 사용자는 존재하지 않습니다."
         )
     
-    is_correct_pw = auth_svc.verify_password(plain_password=req.password,
+    is_correct_pw = auth_svc.verify_password(plain_password=login_info.password,
                                     hashed_password=user.hashed_password)
     
     if not is_correct_pw:
--- App/services/auth_svc.py
+++ App/services/auth_svc.py
@@ -15,7 +15,7 @@ def get_hashed_password(password: str):
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
-# 데이터베이스 통신 (조회, 가입, 인증)
+# 회원가입 시, 이미 존재하는 이메일과 중복 확인
 async def get_user_by_email(conn: Connection, email: str):
     try:
         query = f"""
@@ -44,15 +44,18 @@ async def get_user_by_email(conn: Connection, email: str):
         print(e)
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             detail="알수없는 이유로 서비스 오류가 발생하였습니다.")
-                        
+
+# 실제 DB에 회원 정보(이름, 이메일, 해싱된 패스워드)
 async def register_user(conn: Connection, name: str, email:str, hashed_password: str):
     try:
         query = f"""
         INSERT INTO user(name, email, hashed_password)
-        values ('{name}', '{email}', '{hashed_password}')
+        values (:name, :email, :hashed_password)
         """
-        print("query:", query)
-        await conn.execute(text(query))
+        stmt = text(query)
+        bind_stmt = stmt.bindparams(name=name, email=email, hashed_password=hashed_password)
+        
+        await conn.execute(bind_stmt)
         await conn.commit()
 
     except SQLAlchemyError as e:
@@ -61,7 +64,7 @@ async def register_user(conn: Connection, name: str, email:str, hashed_password:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="요청데이터가 제대로 전달되지 않았습니다")
     
-# 비즈니스 로직 (로그인 시 인증 맞는지 확인)
+# 해당 이메일 가진 회원정보 가져오기
 async def authenticate_user(conn: Connection, email: str):
     """로그인 시 이메일을 검증하여 일치하면 유저 정보를 반환합니다."""
     try:
@@ -72,7 +75,6 @@ async def authenticate_user(conn: Connection, email: str):
         stmt = text(query).bindparams(email=email)
         result = await conn.execute(stmt)
 
-        
         if result.rowcount == 0:
             return False 
         
