b22aa7e7f24bd8ae3b3c597002f519343489cb4e
seoyunje <origin6165@naver.com>
2026-04-28 18:39:55 +0900

    ♻️ Refactor register_image_result

--- App/services/inference_svc.py
+++ App/services/inference_svc.py
@@ -88,30 +88,35 @@ async def predict_image(image_loc: str, version_type: str, model_type: str, doma
             detail="이미지 분석 중 치명적인 오류가 발생했습니다."
         )
 
-# 이미지 메타데이터 + 추론 결과값 DB에 저장
-async def register_image_result(conn: Connection, user_id: int, image_loc: str, score: float, face_conf: float, face_ratio: float,
-                                face_brightness: float, version_type: str, model_type: str, domain_type: str, result_msg: str):
-    if score > 0.5: # 0.5 ~ 1.0 구간 
-        label = "FAKE"
-    elif score >= 0: # 0.0 ~ 0.5 구간
-        label = "REAL"
-    else: # score가 -1인 경우 
-        label = "UNKNOWN" 
-        
+# 빈 이미지 DB 생성 이후, image_id 반환(접수 완료)
+async def register_image_result(conn: Connection, user_id: int | None, image_loc: str, 
+                                version_type: str, model_type: str, domain_type: str):
     try:
         query = """
-        INSERT INTO image_result(user_id, image_loc, label, score, face_conf, face_ratio, face_brightness, version_type, model_type, domain_type, result_msg)
-        values (:user_id, :image_loc, :label, :score, :face_conf, :face_ratio, :face_brightness, :version_type, :model_type, :domain_type, :result_msg)
+            INSERT INTO image_result (
+                user_id, image_loc, status, label, score, face_conf, face_ratio,
+                face_brightness, version_type, model_type, domain_type, result_msg
+            )
+            VALUES (
+                :user_id, :image_loc, 'PENDING', 'UNKNOWN', -1.0, -1.0, -1.0,
+                -1.0, :version_type, :model_type, :domain_type, '분석 대기 중...')
         """
         
         stmt = text(query)
-        bind_stmt = stmt.bindparams(user_id=user_id, image_loc=image_loc, label=label, score=score, face_conf=face_conf, face_ratio=face_ratio,
-                                    face_brightness=face_brightness, version_type=version_type, model_type=model_type, domain_type=domain_type, result_msg=result_msg)
-        await conn.execute(bind_stmt)
+        result = await conn.execute(stmt, {
+            "user_id": user_id, 
+            "image_loc": image_loc,
+            "version_type": version_type,
+            "model_type": model_type,
+            "domain_type": domain_type
+        })
         await conn.commit()
         
+        # MySQL에서 방금 생성된 auto_increment ID 가져오기
+        return result.lastrowid
+        
     except SQLAlchemyError as e:
-        print(e)
+        print(f"DB Insert Error: {e}")
         await conn.rollback()
         await image_svc.delete_image(image_loc)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
