2f65d9c3ba203f5c0f6c8e5105d811d170823f9e
seoyunje <origin6165@naver.com>
2026-04-30 18:56:08 +0900

    ✨ Add BinaryClassifierOutputTarget

---
+++ visualizer/utils/model_targets.py
@@ -0,0 +1,11 @@
+class BinaryClassifierOutputTarget:
+    def __init__(self, category):
+        self.category = category
+
+    def __call__(self, model_output):
+        if self.category == 1:
+            sign = 1
+        else:
+            sign = -1
+        return model_output * sign
+    
