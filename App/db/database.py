2e01d510dab3f55143cffef34cdd6fbddc265eea
seoyunje <origin6165@naver.com>
2026-05-03 10:31:21 +0900

    🔥 Remove dead code: direct_get_conn

--- App/db/database.py
+++ App/db/database.py
@@ -14,7 +14,6 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_CONN = f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-print("database_conn: ", DATABASE_CONN)
 
 engine: AsyncEngine = create_async_engine(
     url = DATABASE_CONN,
@@ -23,16 +22,6 @@ engine: AsyncEngine = create_async_engine(
     max_overflow = 2, 
     pool_recycle = 3600 # 1hour 
 )
-
-async def direct_get_conn():
-    conn = None
-    try:
-        conn = await engine.connect()
-        return conn
-    except SQLAlchemyError as e:
-        print(e)
-        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-                            detail="요청하신 서비스가 잠시 내부적으로 문제가 발생하였습니다")
 
 async def context_get_conn():
     conn = None
