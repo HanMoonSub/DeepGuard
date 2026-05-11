36b504964b1498b1aee4d7d50dc2f089ec971800
seoyunje <origin6165@naver.com>
2026-05-11 10:07:54 +0900

    ⚰️ Remove AblationCAM

--- explainability/perturbation_explainer.py
+++ explainability/perturbation_explainer.py
@@ -1,24 +1,5 @@
-from pytorch_grad_cam import AblationCAM, ScoreCAM, FEM
+from pytorch_grad_cam import ScoreCAM, FEM
 from explainability.cam_explainer import CAMExplainer
-
-
-class AblationCAMExplainer(CAMExplainer):
-    """
-    Zero out activations and measure how the output drops
-    (this repository includes a fast batched implementation)
-    """
-    def __init__(self, batch_size: int = 32, ratio_channels_to_ablate: float = 1.0, **kwargs):
-        self.batch_size = batch_size
-        self.ratio_channels_to_ablate = ratio_channels_to_ablate
-        super().__init__(**kwargs)
-
-    def _build_cam(self):
-        return AblationCAM(model=self.model, target_layers=self.target_layers, reshape_transform=self.reshape_fn,
-                           batch_size=self.batch_size, ratio_channels_to_ablate=self.ratio_channels_to_ablate)
-    def __repr__(self):
-        return super().__repr__().rstrip(")") + f", ratio={self.ratio_channels_to_ablate})"
-
-
 class ScoreCAMExplainer(CAMExplainer):
     """
     Perbutate the image by the scaled activation
