01cc4fe1294d8d67c6865396c788fa9f190bb5a7
seoyunje <origin6165@naver.com>
2026-05-12 19:13:01 +0900

    ✨ Create CAMEvaluator

---
+++ explainability/cam_evaluator.py
@@ -0,0 +1,42 @@
+import numpy as np
+from typing import List, Dict, Literal
+from explainability.utils.model_targets import BinaryClassifierOutputSigmoidTarget
+from explainability.cam_explainer import CAMExplainer
+from explainability.metrics.road import (
+    ROADLeastRelevantFirstAverage, ROADMostRelevantFirstAverage,
+    ROADCombined
+)
+
+METRIC_REGISTRY = {
+    "MORF": ROADMostRelevantFirstAverage,
+    "LERF": ROADLeastRelevantFirstAverage,
+    "Combined": ROADCombined,
+}
+
+class CAMEvaluator:
+    def __init__(self,
+                 cam_explainer: CAMExplainer,
+                 cam_metric: Literal["Combined", "MORF", "LERF"] = "Combined", # Combined, MORF, LERF
+                 percentiles: List[int] = [10,20,30,40,50,60,70,80,90],
+                 ):
+        self.cam_explainer = cam_explainer
+        self.cam_metric_name = cam_metric
+        self.percentiles = percentiles
+        self.cam_metric = METRIC_REGISTRY[cam_metric](percentiles)
+        
+    def evaluate(self, img_path: str) -> float:
+        grayscale_cam, tensor, _ = self.cam_explainer._build_grayscale_cam(img_path)
+        targets = [BinaryClassifierOutputSigmoidTarget(self.cam_explainer.category)]
+        
+        return self.cam_metric(
+            input_tensor = tensor, # (1,C,H,W)
+            cams = grayscale_cam[np.newaxis, ...], #(1,H,W)
+            targets = targets,
+            model = self.cam_explainer.model, 
+        )[0]
+    
+    def __repr__(self):
+        return (
+            f"CAMEvaluator(explainer={self.cam_explainer.__class__.__name__}, "
+            f"metric={self.cam_metric_name}, percentiles={self.percentiles})"
+        )
