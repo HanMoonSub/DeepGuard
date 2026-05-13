d67a42638a97bbc3a447438f77b160586ef92d4e
seoyunje <origin6165@naver.com>
2026-05-13 09:32:29 +0900

    🎨 Add Reproductability for same result

--- explainability/cam_evaluator.py
+++ explainability/cam_evaluator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from typing import List, Dict, Literal
 from explainability.utils.model_targets import BinaryClassifierOutputSigmoidTarget
 from explainability.cam_explainer import CAMExplainer
@@ -18,13 +19,18 @@ class CAMEvaluator:
                  cam_explainer: CAMExplainer,
                  cam_metric: Literal["Combined", "MORF", "LERF"] = "Combined", # Combined, MORF, LERF
                  percentiles: List[int] = [10,20,30,40,50,60,70,80,90],
+                 seed: int = 2026
                  ):
+        self.seed = seed
         self.cam_explainer = cam_explainer
         self.cam_metric_name = cam_metric
         self.percentiles = percentiles
         self.cam_metric = METRIC_REGISTRY[cam_metric](percentiles)
         
     def evaluate(self, img_path: str) -> float:
+        torch.manual_seed(self.seed)
+        np.random.seed(self.seed)
+        
         grayscale_cam, tensor, _ = self.cam_explainer._build_grayscale_cam(img_path)
         targets = [BinaryClassifierOutputSigmoidTarget(self.cam_explainer.category)]
         
